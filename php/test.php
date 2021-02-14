10ca99c1208294b17be9722d9a9324ac01d7e326
sarwar_sikder <amin.se.aiub@gmail.com>
2021-02-14 22:41:25 +0600

    php basic

---
+++ php/test.php
@@ -0,0 +1,90 @@
+<?php
+
+	//output -
+	//print "Hello php..."." abc";	
+	//print('hello php...');
+	//echo "HELLO PHP"." test.."." dkdg dldg ";
+
+	/*$a = 10;
+	$name = 'alamin';
+	$cgpa = 2.5;*/
+	//echo $a;
+
+	//$student = array(1, 'alamin', 2.3, 'alamin@aiub.edu');
+	//$student = [1, 'alamin', 2.3, 'alamin@aiub.edu'];
+
+	/*$students = [
+					[1, 'alamin', 2.3, 'alamin@aiub.edu'],
+					[2, 'xyz', 4, 'xyz@aiub.edu'],
+					[3, 'pqr', 3.3, 'pqr@aiub.edu']
+				];
+
+	echo $students[2][3];*/
+
+	/*$student = ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.3, 'email'=>'alamin@aiub.edu'];
+	echo $student['name'];*/
+
+	/*$students = [
+					['id'=>1, 'name'=>'alamin', 'cgpa'=>2.3, 'email'=>'alamin@aiub.edu'],
+					['id'=>1, 'name'=>'xyz', 'cgpa'=>2.3, 'email'=>'xyz@aiub.edu'],
+					['id'=>1, 'name'=>'pqr', 'cgpa'=>2.3, 'email'=>'pqr@aiub.edu']
+				];
+
+	echo $students[2]['email'];*/
+
+	/*$students = [
+					"s1"=> ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.3, 'email'=>'alamin@aiub.edu'],
+					"s2"=> ['id'=>1, 'name'=>'xyz', 'cgpa'=>2.3, 'email'=>'xyz@aiub.edu'],
+					"s3"=> ['id'=>1, 'name'=>'pqr', 'cgpa'=>2.3, 'email'=>'pqr@aiub.edu']
+				];
+
+	echo $students['s2']['name'];*/
+
+	/*$a = 20;
+
+	if($a === "20"){
+		echo "true";
+	}else{
+		echo "false";
+	}*/
+
+
+	//$student = ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.3, 'email'=>'alamin@aiub.edu'];
+	//$student = [1, 'alamin', 2.3, 'alamin@aiub.edu'];
+
+/*	foreach ($student as $value) {
+		echo $value; 
+		echo " | ";
+	}*/
+
+	/*foreach ($student as $key => $value) {
+		echo $key;
+		echo " | ";
+	}*/
+
+	/*for ($i=0; $i < count($student) ; $i++) { 
+		echo $student[$i];
+	}*/
+
+	/*function sum($a=0, $b=0){
+		return $a+$b;
+	}
+
+	echo sum(5,5);*/
+
+/*	$myfile = fopen('abc.txt', 'r');
+	$data	= fread($myfile, filesize('abc.txt'));
+	echo $data;
+	fclose($myfile);*/
+
+	/*$myfile = fopen('abc.txt', 'w');
+	fwrite($myfile, 'this is file write example....');
+	echo "done!";
+	fclose($myfile);*/
+
+	/*$myfile = fopen('abc.txt', 'a');
+	fwrite($myfile, '\r\n this is file append example....');
+	echo "done!";
+	fclose($myfile);*/
+
+?>
