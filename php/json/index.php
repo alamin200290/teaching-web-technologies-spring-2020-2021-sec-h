5f2dc5eea8f19b04314a11a603c0eae100368b0d
sarwar_sikder <amin.se.aiub@gmail.com>
2021-03-02 11:42:11 +0600

    json example

---
+++ php/json/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/*	$myfile = fopen('users.json', 'r');
+	$data = fread($myfile, filesize('users.json'));
+	
+	$data = '{
+				"id" : 12,
+				"name": "alamin",
+				"email": "alamin@aiub.edu"
+			}';
+
+	$user = json_decode($data, true);
+	print_r($user[1]);*/
+
+	/*$user = [
+				'id' => 11,
+				'name' => 'alamin',
+				'email' => 'abc@gmail.com',
+		 	];
+
+	$data = json_encode($user);
+	echo $data;*/
+?>
